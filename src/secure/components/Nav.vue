98d9b338e8902847efb5946eb7e68b5d273ef46a
Alexx1088 <alexx1088@gmail.com>
2022-12-14 22:21:09 +0600

    add logout

---
+++ src/secure/components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+    <!--
+        <nav class="my-2 my-md-0 mr-md-3">
+          <router-link to="/profile" class="p-2 text-white">{{ user.name }}</router-link>-->
+    <ul class="navbar-nav px-3">
+      <li class="nav-item text-nowrap">
+        <a class="nav-link" href="javascript:void(0)" @click="logout">Sign out</a>
+      </li>
+    </ul>
+  </nav>
+
+</template>
+
+<script>
+import {useRouter} from "vue-router";
+
+export default {
+  name: "Nav",
+  setup() {
+    const router = useRouter();
+
+    const logout = () => {
+      localStorage.clear();
+      router.push('/login')
+    }
+
+    return {
+      logout
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
