dae0f60229948f2cffc957277c68c0660bd52b2a
Alexx1088 <alexx1088@gmail.com>
2022-12-16 19:01:59 +0600

    add logout

---
+++ src/secure/users/Users.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Role</th>
+        <th scope="col">Action</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{user.id}}</td>
+        <td>{{user.first_name}} {{user.last_name}}</td>
+        <td>{{user.email}}</td>
+        <td>{{user.role.name}}</td>
+        <td></td>
+      </tr>
+
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import {ref, onMounted} from 'vue';
+import axios from "axios";
+
+export default {
+  name: "Users",
+
+  setup() {
+    const users = ref([]);
+
+    onMounted(async () => {
+      const response = await axios.get('users');
+
+      users.value = response.data.data;
+    });
+    return {
+      users
+    }
+  }
+}
+</script>
+
