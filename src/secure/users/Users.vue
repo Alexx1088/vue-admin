b097fc6dbb877c124ec790329a8f8a5ba22ba1e9
Alexx1088 <alexx1088@gmail.com>
2022-12-19 19:10:22 +0600

    add creating users

--- src/secure/users/Users.vue
+++ src/secure/users/Users.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <div class="btn-toolbar mb-2 mb-md-0" >
+      <router-link to="/users/create" class="btn btn-sm btn-outline-secondary">Add</router-link>
+    </div>
+  </div>
   <div class="table-responsive">
     <table class="table table-striped table-sm">
       <thead>
@@ -16,7 +21,14 @@
         <td>{{user.first_name}} {{user.last_name}}</td>
         <td>{{user.email}}</td>
         <td>{{user.role.name}}</td>
-        <td></td>
+        <td>
+          <div class="btn-group mr-2">
+            <a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary">Edit</a>
+            <a href="javascript:void(0)" class="btn btn-sm btn-outline-secondary" @click="del(user.id)">Delete</a>
+          </div>
+
+
+        </td>
       </tr>
       </tbody>
     </table>
@@ -36,6 +48,8 @@
 <script lang="ts">
 import {ref, onMounted} from 'vue';
 import axios from "axios";
+/*import {User} from "@/classes/user";*/
+import {Entity} from "@/interfaces/entity";
 
 export default {
   name: "Users",
@@ -46,11 +60,10 @@ export default {
     const lastPage = ref(0);
 
     const load = async () => {
-      const response = await axios.get('users?page=${page.value}');
+      const response = await axios.get(`users?page=${page.value}`);
       users.value = response.data.data;
       lastPage.value = response.data.meta.last_page;
     }
-    onMounted(load);
 
 const next = async () => {
   if (page.value === lastPage.value) return;
@@ -65,10 +78,21 @@ page.value--;
 await load();
     }
 
+    const del = async (id: number) => {
+if (confirm('Are you sure you want to delete this record?')) {
+  await axios.delete(`users/${id}`);
+
+  users.value = users.value.filter((e: Entity) => e.id !== id);
+}
+    }
+
+    onMounted(load);
+
     return {
       users,
       next,
-      prev
+      prev,
+      del
     }
   }
 }
